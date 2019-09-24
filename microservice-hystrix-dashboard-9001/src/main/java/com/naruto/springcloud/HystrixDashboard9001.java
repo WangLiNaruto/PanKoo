50455a5a3c0a4ba86dbbda656e8f2b42ee531458
naruto <1456017852@qq.com>
2019-09-24 23:12:30 +0800

    springcloud-hystrix-dashboard init

--- microservice-hystrix-dashboard-9001/src/main/java/com/naruto/springcloud/HystrixDashboard9001.java
+++ microservice-hystrix-dashboard-9001/src/main/java/com/naruto/springcloud/HystrixDashboard9001.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-
+//localhost:9001/hystrix.stream
 @SpringBootApplication
 @EnableHystrixDashboard
 public class HystrixDashboard9001 {
